bd69ec93a54e1de74c189dd21554cf2963767206
Linus Fredriksson <linfredriksson@gmail.com>
2016-11-21 22:43:31 +0100

    - Add example menu script for Agisoft PhotoScan.

---
+++ source/agisoft_example_menu.py
@@ -0,0 +1,22 @@
+import PhotoScan
+
+def function1():
+    """
+    Example function 1
+    """
+    doc = PhotoScan.app.document
+    print("Running function1")
+    return 1
+
+def function2():
+    """
+    Example function 2
+    """
+    doc = PhotoScan.app.document
+    print("Running function2")
+    return 1
+
+label = "ExampleMenu/ExampleScript1"
+PhotoScan.app.addMenuItem(label, function1, "ctrl+alt+i")
+label = "ExampleMenu/ExampleScript2"
+PhotoScan.app.addMenuItem(label, function2, "ctrl+alt+o")
