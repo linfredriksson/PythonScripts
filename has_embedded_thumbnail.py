0fd57dfbddd57adaa120665f05f0319cbc51bb67
Linus Fredriksson <linfredriksson@gmail.com>
2020-02-15 17:04:36 +0100

    Create has_embedded_thumbnail.py
    Add script for checking if a video has a embedded thumbnail image.

---
+++ has_embedded_thumbnail.py
@@ -0,0 +1,45 @@
+import subprocess
+import json
+import sys
+import os
+
+
+def runSubprocess(command): 
+    return subprocess.check_output(command)
+
+
+def readMetaAsJson(path):
+    result = runSubprocess(["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", path])
+    return json.loads(result)
+
+
+def hasEmbeddedThumbnail(path):
+    data = readMetaAsJson(sys.argv[1])
+    if "streams" not in data:
+        print("Unable to read streams")
+        return False
+    for stream in data["streams"]:
+        if "tags" not in stream:
+            continue
+        if "filename" not in stream["tags"]:
+            continue
+        if stream["tags"]["filename"] == "":
+            continue
+        return True
+    return False
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Missing arguments")
+        print("Example Use: 'python filename.py path/to/video'")
+        return
+    if not os.path.exists(sys.argv[1]):
+        print("File not found")
+        return
+    if hasThumbnailEmbedded(sys.argv[1]):
+        print("Thumbnail embedded")
+    else:
+        print("No Thumbnail embedded")
+
+main()
